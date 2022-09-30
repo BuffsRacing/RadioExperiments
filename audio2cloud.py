43093bef43e59bba5b5c09f377f53e79b6cb0932
navanchauhan <navanchauhan@gmail.com>
2022-09-30 16:41:08 -0600

    poetry env

--- audio2cloud.py
+++ audio2cloud.py
@@ -1,7 +1,10 @@
 from sys import byteorder
 from array import array
 from struct import pack
+from dotenv import load_dotenv
 
+import azure.cognitiveservices.speech as speechsdk
+import os
 import pyaudio
 import time
 import wave
@@ -57,7 +60,7 @@ def add_silence(snd_data, seconds):
     r.extend(silence)
     return r
 
-def record():
+def record(usb_audio_index):
     """
     Record a word or words from the microphone and 
     return the data as an array of signed shorts.
@@ -69,7 +72,7 @@ def record():
     """
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True, input_device_index=1,
+        input=True, output=True, input_device_index=usb_audio_index,
         frames_per_buffer=CHUNK_SIZE)
 
     num_silent = 0
@@ -79,7 +82,7 @@ def record():
 
     while 1:
         # little endian, signed short
-        snd_data = array('h', stream.read(CHUNK_SIZE))
+        snd_data = array('h', stream.read(CHUNK_SIZE,exception_on_overflow = False)) # exception_on_overflow = False is because of some weird bug on macOS
         if byteorder == 'big':
             snd_data.byteswap()
         r.extend(snd_data)
@@ -104,9 +107,9 @@ def record():
     r = add_silence(r, 0.5)
     return sample_width, r
 
-def record_to_file(path):
+def record_to_file(path, usb_audio_index):
     "Records from the microphone and outputs the resulting data to 'path'"
-    sample_width, data = record()
+    sample_width, data = record(usb_audio_index)
     data = pack('<' + ('h'*len(data)), *data)
 
     wf = wave.open(path, 'wb')
@@ -116,11 +119,42 @@ def record_to_file(path):
     wf.writeframes(data)
     wf.close()
 
+def from_file(audio_fname, speech_key, speech_region):
+    speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_region)
+    audio_input = speechsdk.AudioConfig(filename=audio_fname)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
+
+    result = speech_recognizer.recognize_once_async().get()
+    print(f"Radio Transcript: {result.text}")
+
 def natural_fname():
     return time.strftime("%Y%m%d_%H%M%S.wav", time.localtime())
 
+
+
 if __name__ == '__main__':
+    transcribe = True
+    load_dotenv()
+    speech_key = os.getenv("AZURE_SPEECH_KEY")
+    speech_region = os.getenv("AZURE_SPEECH_REGION")
+    usb_audio_index = os.getenv("USB_AUDIO_INDEX")
+    if speech_key is None or speech_region is None:
+        print("Please set the AZURE_SPEECH_KEY and AZURE_SPEECH_REGION environment variables.")
+        print("Disabling speech recognition.")
+        transcribe = False
+    if usb_audio_index is None:
+        print("USB_AUDIO_INDEX enviornment variable not set.")
+        print("Falling back to default (0)")
+        usb_audio_index = 0
+    try:
+        usb_audio_index = int(usb_audio_index)
+    except:
+        print(f"Error converting {usb_audio_index} to int")
+        usb_audio_index  = 0
     while True:
         print("Waiting for radio message...")
-        record_to_file(f"BuffsRacing {natural_fname()}.wav")
-        print("Recorded radio message.")
+        audio_fname = f"BuffsRadio_{natural_fname()}"
+        record_to_file(audio_fname, usb_audio_index)
+        if transcribe:
+            from_file(audio_fname,speech_key,speech_region)
+        print("Recorded radio message.") 
